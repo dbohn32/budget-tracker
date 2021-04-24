ad79206c08fadc1becfb75a56abae3aa95bd3d6e
Dan Bohn <dbohn32@yahoo.com>
2021-04-23 21:12:34 -0400

    deploying

--- server.js
+++ server.js
@@ -16,12 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 app.use(apiRoutes);
 
